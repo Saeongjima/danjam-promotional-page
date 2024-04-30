a7437f3600b9fa9ad110d180220101d215aacb0e
나강민 <139530542+KangminNa@users.noreply.github.com>
2024-04-30 14:28:02 +0900

    feat : section5 구현완료

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let ranges = [
-    { start: 300, end: 1800, elements: '.section2-shortline, .section2-group' },
-    { start: 1500, end: 3000, elements: '.section3-group, .section3-down-arrow, .section3-shortline' },
-    { start: 2500, end: 4000, elements: '.section4-shortline, .section4-group1, .section4-group2, .section4-group3'}
+    { start: 1000, end: 3000, elements: '.section2-shortline, .section2-group' },
+    { start: 2000, end: 4000, elements: '.section3-group, .section3-down-arrow, .section3-shortline' },
+    { start: 3000, end: 5000, elements: '.section4-shortline, .section4-group1, .section4-group2, .section4-group3'},
+    { start: 4000, end: 6000, elements: '.single-image, .section5-table1, .section5-table2, .section5-table3, .section5-table4, .section5-table5, .section5-table6, .section5-table7, .section5-table8, .section5-table9, .section5-table10'}
 ];
+
 
 let animationExecuted = false; // 애니메이션이 실행되었는지 여부를 추적하는 변수
 
